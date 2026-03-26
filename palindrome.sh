20089c535d793a8ae0efb4ad501bb795ea8e22c1
montuvlv <151778679+Montuvlv@users.noreply.github.com>
2026-03-26 12:47:54 +0530

    Add palindrome check script in palindrome.sh

---
+++ palindrome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read -p "Enter a number or string: " input
+reverse=$(echo "$input" | rev)
+if [ "$input" == "$reverse" ]; then
+echo "$input is a palindrome."
+else
+echo "$input is not a palindrome."
+fi
